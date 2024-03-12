0cb84b8afb61c2cfc44f954d5047d4c2bcd02724
이예원 <2_yewon@naver.com>
2024-03-12 13:21:54 +0900

    [init] 프로젝트 초기 설정

---
+++ Backend/src/main/java/com/ssafy/pennypal/PennypalApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.pennypal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PennypalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PennypalApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/ssafy/pennypal/PennypalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.pennypal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PennypalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
