57514d233da4aac6716c6dc7f19dec2649f04aad
이예원 <2_yewon@naver.com>
2024-03-20 20:35:04 +0900

    [feat] 팀 생성 기능 개발 및 REST docs 생성

--- Backend/src/test/java/com/ssafy/pennypal/domain/team/controller/TeamControllerTest.java
+++ Backend/src/test/java/com/ssafy/pennypal/domain/team/controller/TeamControllerTest.java
@@ -1,8 +1,12 @@
 package com.ssafy.pennypal.domain.team.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ssafy.pennypal.common.RestDocsSupport;
 import com.ssafy.pennypal.domain.member.entity.Member;
+import com.ssafy.pennypal.domain.member.repository.IMemberRepository;
 import com.ssafy.pennypal.domain.team.dto.request.TeamCreateRequest;
+import com.ssafy.pennypal.domain.team.dto.request.TeamCreateServiceRequest;
+import com.ssafy.pennypal.domain.team.dto.response.TeamCreateResponse;
 import com.ssafy.pennypal.domain.team.service.TeamService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -20,71 +24,89 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(controllers = TeamController.class)
-class TeamControllerTest {
+public class TeamControllerTest extends RestDocsSupport {
 
-    @Autowired
-    private MockMvc mockMvc;
+    @Override
+    protected Object initController() {
+        return new TeamController(teamService);
+    }
 
-    @Autowired
-    private ObjectMapper objectMapper;
+    private final TeamService teamService = mock(TeamService.class);
 
-    @MockBean
-    private TeamService teamService;
 
-    // todo : 강의 듣고 다시 작성
-//    @DisplayName("팀을 생성한다.")
-//    @Test
-//    void createTeam() throws Exception {
-//        // given
-//
-//        Member member = createMember("member1@pennypal.site", "짠1", LocalDateTime.now());
-//
-//        TeamCreateRequest request = TeamCreateRequest.builder()
-//                .teamName("팀이름1")
-//                .teamIsAutoConfirm(false)
-//                .teamLeaderId(member.getMemberId())
-//                .teamInfo("팀소개입니다.")
-//                .build();
-//
-//        // when, then
-//        mockMvc.perform(
-//                        post("/team/api/create")
-//                                .contentType(MediaType.APPLICATION_JSON)
-//                )
-//                .andDo(print())
-//                .andExpect(status().isOk())
-//                .andDo(document("create-team",preprocessRequest(prettyPrint()),
-//                        preprocessResponse(prettyPrint()),
-//                        responseFields(
-//                                fieldWithPath("code").type(JsonFieldType.NUMBER)
-//                                        .description("코드"),
-//                                fieldWithPath("status").type(JsonFieldType.STRING)
-//                                        .description("상태"),
-//                                fieldWithPath("message").type(JsonFieldType.STRING)
-//                                        .description("메시지"),
-//                                fieldWithPath("data").type(JsonFieldType.OBJECT)
-//                                        .description("응답 데이터"),
-//                                fieldWithPath("data.teamName").type(JsonFieldType.STRING)
-//                                        .description("팀 명"),
-//                                fieldWithPath("data.teamIsAutoConfirm").type(JsonFieldType.BOOLEAN)
-//                                        .description(" 자동가입 승인 여부"),
-//                                fieldWithPath("data.teamLeaderId").type(JsonFieldType.NUMBER)
-//                                        .description("팀장 Id"),
-//                                fieldWithPath("data.teamInfo").type(JsonFieldType.STRING)
-//                                        .description("팀 한줄소개")
-//                        )
-//                )
-//        );
-//
-//    }
+    @DisplayName("팀 생성")
+    @Test
+    void createTeam() throws Exception {
+
+        Member mockMember = mock(Member.class);
+
+        // ID 1L의 Mock Member 생성
+        given(mockMember.getMemberId()).willReturn(1L);
+
+        TeamCreateRequest request = TeamCreateRequest.builder()
+                .teamName("팀이름")
+                .teamIsAutoConfirm(false)
+                .teamLeaderId(mockMember.getMemberId())
+                .teamInfo("팀소개")
+                .build();
+
+        // stubbing
+        given(teamService.createTeam(any(TeamCreateServiceRequest.class)))
+                .willReturn(TeamCreateResponse.builder()
+                        .teamName("팀이름")
+                        .teamIsAutoConfirm(false)
+                        .teamLeaderId(1L)
+                        .teamInfo("팀소개")
+                        .build());
+
+        mockMvc.perform(
+                        post("/api/team/create")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(new ObjectMapper().writeValueAsString(request))
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("create-team",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        requestFields(
+                                fieldWithPath("teamName").type(JsonFieldType.STRING)
+                                        .description("팀 이름"),
+                                fieldWithPath("teamIsAutoConfirm").type(JsonFieldType.BOOLEAN)
+                                        .description("자동가입 승인 여부"),
+                                fieldWithPath("teamLeaderId").type(JsonFieldType.NUMBER)
+                                        .description("팀장 ID"),
+                                fieldWithPath("teamInfo").type(JsonFieldType.STRING)
+                                        .description("팀 한줄소개")
+                        ),
+                        responseFields(
+                                fieldWithPath("code").type(JsonFieldType.NUMBER)
+                                        .description("코드"),
+                                fieldWithPath("status").type(JsonFieldType.STRING)
+                                        .description("상태"),
+                                fieldWithPath("message").type(JsonFieldType.STRING)
+                                        .description("메시지"),
+                                fieldWithPath("data").type(JsonFieldType.OBJECT)
+                                        .description("응답 데이터"),
+                                fieldWithPath("data.teamName").type(JsonFieldType.STRING)
+                                        .description("팀 명"),
+                                fieldWithPath("data.teamIsAutoConfirm").type(JsonFieldType.BOOLEAN)
+                                        .description(" 자동가입 승인 여부"),
+                                fieldWithPath("data.teamLeaderId").type(JsonFieldType.NUMBER)
+                                        .description("팀장 ID"),
+                                fieldWithPath("data.teamInfo").type(JsonFieldType.STRING)
+                                        .description("팀 한줄소개")
+                        )
+                )
+        );
+
+    }
 
     private Member createMember(String memberEmail, String memberNickname, LocalDateTime memberBirthDate){
         return Member.builder()
@@ -94,7 +116,7 @@ class TeamControllerTest {
                 .memberNickname(memberNickname)
                 .memberBirthDate(memberBirthDate)
                 .build();
-    }
 
+    }
 
 }
