6f0da6852201044511f3f192c055bd9105f77fcc
조한빈 <whgksqls9999@naver.com>
2024-03-21 17:13:19 +0900

    [design] 팀 인포 멤버 지출액 CSS 적용중

--- Frontend/src/pages/teamInfo/ui/TeamInfoMemberItem/TeamInfoMemberItem.tsx
+++ Frontend/src/pages/teamInfo/ui/TeamInfoMemberItem/TeamInfoMemberItem.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { usePenny } from '../../../../shared';
 
 type TeamInfoMemberItem = {
     nickname: string;
@@ -7,6 +8,8 @@ type TeamInfoMemberItem = {
 };
 
 export function TeamInfoMemberItem({ nickname, isLeader, spend }: TeamInfoMemberItem) {
+    const penny = usePenny();
+
     return (
         <div className="teamInfoMemberItem">
             <div className="teamInfoMemberItem__top">
@@ -14,7 +17,7 @@ export function TeamInfoMemberItem({ nickname, isLeader, spend }: TeamInfoMember
                 {isLeader ? <div className="teamInfoMemberItem__top-leader"></div> : null}
             </div>
             <div className="teamInfoMemberItem__bottom">
-                <div className="teamInfoMemberItem__bottom-spend">{spend.toLocaleString()}</div>
+                <div className="teamInfoMemberItem__bottom-spend">{penny.toFullNumber(spend)}</div>
             </div>
         </div>
     );
