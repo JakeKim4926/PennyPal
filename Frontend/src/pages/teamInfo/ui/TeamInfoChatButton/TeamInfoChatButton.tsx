8a474b9bc43788a4f4e180ebaccd8d8a15ae2b00
조한빈 <whgksqls9999@naver.com>
2024-03-25 11:01:00 +0900

    [design] 팀 인포 채팅 버튼 UI 구현

---
+++ Frontend/src/pages/teamInfo/ui/TeamInfoChatButton/TeamInfoChatButton.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function TeamInfoChatButton() {
+    return (
+        <div className="teamInfoChatButton">
+            <button className="teamInfoChatButton__button">채팅창</button>
+        </div>
+    );
+}
