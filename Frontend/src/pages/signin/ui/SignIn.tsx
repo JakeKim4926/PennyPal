f9fba812d44fc7724ac80bfb6c4280c573fc3430
김동학 <kdh19583@gmail.com>
2024-03-25 17:57:10 +0900

    [feat] 로그인 ui중

--- Frontend/src/pages/signin/ui/SignIn.tsx
+++ Frontend/src/pages/signin/ui/SignIn.tsx
@@ -1,3 +1,27 @@
 export function SignIn() {
-    return <div>SignIn</div>;
+    return (
+        <div className="signin container">
+            <div className="welcomebox">
+                <p className="welcomebox__text">WELCOME TO PENNYPAL !</p>
+                <div className="welcomebox__logo">
+                    <img src="assets/image/main-logo-colored.svg" />
+                </div>
+                <p className="welcomebox__singIn">SIGN IN</p>
+            </div>
+            <div className="signin-progress container">
+                <div>
+                    <p>E-mail</p>
+                    <div className="input-container">
+                        <img src="assets/image/icons_mini/Email.svg" />
+                        <input type="text" placeholder="이메일" onChange={() => {}} />
+                    </div>
+                    <p>Password</p>
+                    <div className="input-container">
+                        <img src="assets/image/icons_mini/password.svg" />
+                        <input type="password" placeholder="패스워드" onChange={(e) => {}} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
