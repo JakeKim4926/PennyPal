9fe79698f35ed1405db1769f1500b1ad54988198
조한빈 <whgksqls9999@naver.com>
2024-03-27 13:10:12 +0900

    [feat] 마켓 사용내역 마켓 되돌아가기 기능 구현

--- Frontend/src/pages/marketReceipt/ui/MarketReceiptTop/MarketReceiptTop.tsx
+++ Frontend/src/pages/marketReceipt/ui/MarketReceiptTop/MarketReceiptTop.tsx
@@ -1,7 +1,11 @@
+import { Link } from 'react-router-dom';
+
 export function MarketReceiptTop() {
     return (
         <div className="marketReceiptTop">
-            <div className="marketReceiptTop__left">◀ 마켓으로 돌아가기</div>
+            <Link to="/market">
+                <div className="marketReceiptTop__left">◀ 마켓으로 돌아가기</div>
+            </Link>
             <div className="marketReceiptTop__right">
                 <button className="marketReceiptTop__right-button button">[P] 포인트 모으러 가기 →</button>
             </div>
