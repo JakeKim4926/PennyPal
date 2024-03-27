9d4cda88424f89527ed2aea1e099dd58a1a1168d
박시연 <xion2664@gmail.com>
2024-03-28 08:59:40 +0900

    [design] piechart까지

--- Frontend/src/pages/expenditure/ui/ExpenditureGraph/ExpenditureGraph.tsx
+++ Frontend/src/pages/expenditure/ui/ExpenditureGraph/ExpenditureGraph.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+import { ChartArea } from '@/shared';
 
 export function ExpenditureGraph() {
+    const data = [
+        [100000, 10000, 170000, 80000, 100000, 2000, 90000],
+        [50000, 100000, 50000, 50000, 50000, 40000, 60000],
+    ];
+
     return (
         <div className="contentCard">
             <div className="contentCard__title">
                 <div className="contentCard__title-text">GRAPH</div>
             </div>
+            <ChartArea data={data} />
         </div>
     );
 }
