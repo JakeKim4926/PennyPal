39fdb0b9218a08c1f93a83bd10d3ad6e5b98981d
조한빈 <whgksqls9999@naver.com>
2024-03-27 13:08:20 +0900

    [design] 마켓 사용내역 영수증 UI 작업 완료

--- Frontend/src/pages/marketReceipt/ui/MarketReceiptTop/MarketReceiptTop.tsx
+++ Frontend/src/pages/marketReceipt/ui/MarketReceiptTop/MarketReceiptTop.tsx
@@ -3,7 +3,7 @@ export function MarketReceiptTop() {
         <div className="marketReceiptTop">
             <div className="marketReceiptTop__left">◀ 마켓으로 돌아가기</div>
             <div className="marketReceiptTop__right">
-                <button className="marketReceiptTop__right-button">[P] 포인트 모으러 가기 →</button>
+                <button className="marketReceiptTop__right-button button">[P] 포인트 모으러 가기 →</button>
             </div>
         </div>
     );
