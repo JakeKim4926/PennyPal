db68417b9ee86f5bef1a5c53f75537e20fd2efbd
조한빈 <whgksqls9999@naver.com>
2024-03-27 15:58:05 +0900

    [feat] 팀 생성 기능 구현중

--- Frontend/src/pages/team/ui/TeamCreateTeam/TeamCreateTeam.tsx
+++ Frontend/src/pages/team/ui/TeamCreateTeam/TeamCreateTeam.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 export function TeamCreateTeam() {
     return (
@@ -13,14 +13,37 @@ export function TeamCreateTeam() {
 }
 
 function Content() {
-    const [curPage, setCurPage] = useState(0);
+    useEffect(() => {
+        if (contentRef.current) {
+            console.dir(contentRef.current.offsetWidth);
+        }
+    });
+    const contentRef = useRef<HTMLDivElement>(null);
 
     return (
-        <>
-            <div className="teamCreateTeam__content">
-                <div className="teamCreateTeam__content-top">팀을 생성해 함께할 동료들을 모아보아요!</div>
-                <button className="teamCreateTeam__content-nextButton">다음으로</button>
+        <div className="teamCreateTeam__content" ref={contentRef}>
+            <div className="teamCreateTeam__content-inner">
+                <div className="teamCreateTeam__content-inner-top">팀을 생성해 함께할 동료들을 모아보아요!</div>
+                <button
+                    className="teamCreateTeam__content-inner-nextButton"
+                    onClick={() => {
+                        contentRef.current?.scrollTo({
+                            top: contentRef.current?.offsetHeight,
+                            behavior: 'smooth',
+                        });
+                    }}
+                >
+                    팀 생성하기
+                </button>
             </div>
-        </>
+            <div className="teamCreateTeam__content-inner">
+                <div className="teamCreateTeam__content-inner-second">
+                    <div className="teamCreateTeam__content-inner-second-name">
+                        <div>팀명을 입력해주세요</div>
+                        <input type="text" placeholder="팀명"></input>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
