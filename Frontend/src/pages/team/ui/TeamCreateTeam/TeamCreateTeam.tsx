24492b1a8e6dee820009f0b07feead039e39f692
조한빈 <whgksqls9999@naver.com>
2024-03-27 17:17:41 +0900

    [feat] 팀명 유효성 검사 디바운싱 적용 및 버튼 공통 컴포넌트 disabled 적용

--- Frontend/src/pages/team/ui/TeamCreateTeam/TeamCreateTeam.tsx
+++ Frontend/src/pages/team/ui/TeamCreateTeam/TeamCreateTeam.tsx
@@ -37,11 +37,12 @@ function Content() {
                     팀 생성하기
                 </button>
             </div>
+            <NameArea moveNext={() => moveNext(contentRef, 2)} />
             <div className="teamCreateTeam__content-inner">
                 <div className="teamCreateTeam__content-inner-second">
                     <div className="teamCreateTeam__content-inner-second-name">
-                        <div>팀명을 입력해주세요</div>
-                        <input type="text" placeholder="팀명"></input>
+                        <div>우리 팀을 한 줄로 소개해주세요</div>
+                        <input type="text" placeholder="소개 (40자 이내)"></input>
                     </div>
                 </div>
             </div>
@@ -49,18 +50,90 @@ function Content() {
                 <div className="teamCreateTeam__content-inner-second">
                     <div className="teamCreateTeam__content-inner-second-name">
                         <div>우리 팀을 한 줄로 소개해주세요</div>
-                        <input type="text" placeholder="소개"></input>
+                        <input type="text"></input>
                     </div>
                 </div>
-            </div>
-            <div className="teamCreateTeam__content-inner">
-                <div className="teamCreateTeam__content-inner-second">
-                    <div className="teamCreateTeam__content-inner-second-name">
-                        <div>우리 팀을 한 줄로 소개해주세요</div>
-                        <input type="text" placeholder="소개"></input>
-                    </div>
-                </div>
+                <button
+                    onClick={() => {
+                        moveNext(contentRef, 2);
+                    }}
+                >
+                    다음으로
+                </button>
             </div>
         </div>
     );
+}
+
+type NameAreaProps = {
+    moveNext: () => void;
+};
+
+function NameArea({ moveNext }: NameAreaProps) {
+    const [check, setCheck] = useState('INVALID');
+
+    let timer: any = '';
+    const regex = /^[가-힣|a-z|A-Z|0-9|]+$/;
+
+    function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+        const newValue = e.target.value;
+
+        if (timer) {
+            clearTimeout(timer);
+        }
+
+        timer = setTimeout(() => {
+            console.log(newValue, newValue.length);
+            if (newValue.length >= 4 && newValue.length <= 20 && regex.test(newValue)) {
+                setCheck('VALID');
+            } else {
+                if (check !== 'INVALID') {
+                    setCheck('INVALID');
+                }
+            }
+        }, 500);
+    }
+
+    return (
+        <div className="teamCreateTeam__content-inner">
+            <div className="teamCreateTeam__content-inner-second">
+                <div className="teamCreateTeam__content-inner-second-name">
+                    <div>팀명을 입력해주세요</div>
+                    <div className="teamCreateTeam__content-inner-second-name-input">
+                        <input type="text" placeholder="팀명" onChange={handleChange}></input>
+                        <button
+                            className={`teamCreateTeam__content-inner-second-name-input-button ${
+                                check !== 'VALID' ? 'button-disabled' : 'button'
+                            }`}
+                        >
+                            중복 확인
+                        </button>
+                    </div>
+                    <NameCheck state={check} />
+                </div>
+            </div>
+            <button
+                disabled={check !== 'PASS'}
+                onClick={() => {
+                    moveNext();
+                }}
+            >
+                다음으로
+            </button>
+        </div>
+    );
+}
+
+type NameCheckProps = {
+    state: string;
+};
+function NameCheck({ state }: NameCheckProps) {
+    switch (state) {
+        case 'INVALID':
+            return <div className="teamCreateTeam__content-inner-second-name">유효하지 않은 팀명입니다.</div>;
+        case 'VALID':
+            return <div className="teamCreateTeam__content-inner-second-name">유효한 팀명입니다.</div>;
+        case 'PASS':
+            return <div className="teamCreateTeam__content-inner-second-name">사용 가능한 팀명입니다.</div>;
+    }
 }
