f72157f689c0862612f14aae51b5c8fce46d573c
조한빈 <whgksqls9999@naver.com>
2024-03-31 17:37:43 +0900

    [fix] 팀 생성하기 scss 파일 누락부 추가

--- Frontend/src/pages/finance/ui/Finance.tsx
+++ Frontend/src/pages/finance/ui/Finance.tsx
@@ -2,32 +2,31 @@ import { USER_ID, customAxios } from '@/shared';
 import { CompatClient, Stomp } from '@stomp/stompjs';
 import { useEffect, useRef } from 'react';
 import SockJS from 'sockjs-client';
-import StompJS from '@stomp/stompjs';
 
 export function Finance() {
     const client = useRef<CompatClient>();
 
     useEffect(() => {
         const connectHandler = () => {
-            const socket = new SockJS('http://localhost:8080/');
-            // client.current = Stomp.over(socket);
-            // client.current!.connect(
-            //     {},
-            //     () => {
-            //         // 연결 성공 시 실행될 콜백 함수
-            //         client.current!.subscribe(
-            //             '/sub/chat/22', // 수신
-            //             (message) => {
-            //                 // 메시지 수신 시 실행될 콜백 함수
-            //                 alert(message.body);
-            //             },
-            //         );
-            //     },
-            //     (error: any) => {
-            //         // 에러 처리
-            //         console.error('WebSocket 연결 에러:', error);
-            //     },
-            // );
+            const socket = new SockJS('http://localhost:8080/ws');
+            client.current = Stomp.over(socket);
+            client.current!.connect(
+                {},
+                () => {
+                    // 연결 성공 시 실행될 콜백 함수
+                    client.current!.subscribe(
+                        '/sub/chat/22', // 수신
+                        (message) => {
+                            // 메시지 수신 시 실행될 콜백 함수
+                            alert(message.body);
+                        },
+                    );
+                },
+                (error: any) => {
+                    // 에러 처리
+                    console.error('WebSocket 연결 에러:', error);
+                },
+            );
         };
 
         connectHandler(); // 컴포넌트가 마운트될 때 한 번 호출
