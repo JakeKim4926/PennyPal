b6cf8626f1f2801188c3ae3f78265cb61db42d62
조한빈 <whgksqls9999@naver.com>
2024-03-31 17:40:50 +0900

    [feat] 팀 채팅창 리듀서 적용

--- Frontend/src/pages/teamInfo/index.ts
+++ Frontend/src/pages/teamInfo/index.ts
@@ -1,6 +1,9 @@
 export * from '@/pages/teamInfo/ui/TeamInfo';
 export * from '@/pages/teamInfo/ui/TeamLeaveModal/TeamLeavModal';
-export * from '@/pages/teamInfo/model/openTeamLeaveModal';
 export * from '@/pages/teamInfo/ui/TeamSettingModal/TeamSettingModal';
+
+export * from '@/pages/teamInfo/model/openTeamLeaveModal';
 export * from '@/pages/teamInfo/model/openTeamSettingModal';
+export * from '@/pages/teamInfo/model/openTeamChattingModal';
+
 export * from '@/pages/teamInfo/api/deleteTeam';
