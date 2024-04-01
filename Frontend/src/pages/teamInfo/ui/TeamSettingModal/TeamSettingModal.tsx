d615bbe04f1e070af819ee03c40ecbd1aa533e64
조한빈 <whgksqls9999@naver.com>
2024-04-01 10:56:11 +0900

    [design] 팀 정보 수정 레이아웃

--- Frontend/src/pages/teamInfo/ui/TeamSettingModal/TeamSettingModal.tsx
+++ Frontend/src/pages/teamInfo/ui/TeamSettingModal/TeamSettingModal.tsx
@@ -1,5 +1,6 @@
-import { deleteTeam, getTeamWaitingList } from '@/pages/teamInfo/index';
+import { closeTeamSettingModal, deleteTeam, getTeamWaitingList } from '@/pages/teamInfo/index';
 import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 type TeamSettingModal = {
     teamId: number;
@@ -11,6 +12,7 @@ type TeamSettingModal = {
 
 export function TeamSettingModal({ teamId, memberId, teamName, teamInfo, members }: TeamSettingModal) {
     const [waitingList, setWaitingList] = useState([]);
+    const dispatch = useDispatch();
 
     useEffect(() => {
         const postDto = {
@@ -32,32 +34,54 @@ export function TeamSettingModal({ teamId, memberId, teamName, teamInfo, members
     return (
         <div className="modalContainer">
             <div className="teamSettingModal">
-                <div className="teamSettingModal__top"></div>
                 <div className="teamSettingModal__middle">
-                    <div className="teamSettingModal__middle-teamName">
-                        <div className="teamSettingModal__middle-teamName-key">팀명</div>
-                        <div className="teamSettingModal__middle-teamName-value">{teamName}</div>
-                    </div>
-                    <div className="teamSettingModal__middle-teamInfo">
-                        <div className="teamSettingModal__middle-teamInfo-key">팀소개</div>
-                        <input
-                            className="teamSettingModal__middle-teamInfo-value"
-                            defaultValue={teamInfo ?? '팀 소개말이 없습니다.'}
-                        />
-                    </div>
-                    <div className="teamSettingModal__middle-teamIsAutoConfirm">
-                        <div className="teamSettingModal__middle-teamIsAutoConfirm-key">가입 자동 승인 여부</div>
-                        <div className="teamSettingModal__middle-teamIsAutoConfirm-value">
-                            <div>YES</div>
-                            <div>NO</div>
+                    <div className="teamSettingModal__middle-info">
+                        <div className="teamSettingModal__middle-info-teamName">
+                            <div className="teamSettingModal__middle-info-teamName-key">팀명</div>
+                            <div className="teamSettingModal__middle-info-teamName-value">{teamName}</div>
+                        </div>
+                        <div className="teamSettingModal__middle-info-teamInfo">
+                            <div className="teamSettingModal__middle-info-teamInfo-key">팀소개</div>
+                            <input
+                                className="teamSettingModal__middle-info-teamInfo-value"
+                                defaultValue={teamInfo ?? '팀 소개말이 없습니다.'}
+                            />
+                        </div>
+                        <div className="teamSettingModal__middle-info-teamIsAutoConfirm">
+                            <div className="teamSettingModal__middle-info-teamIsAutoConfirm-key">
+                                가입 자동 승인 여부
+                            </div>
+                            <div className="teamSettingModal__middle-info-teamIsAutoConfirm-value">
+                                <div>YES</div>
+                                <div>NO</div>
+                            </div>
+                        </div>
+                        <div className="teamSettingModal__middle-info-modify">
+                            <button className="teamSettingModal__middle-info-modify-button">수정하기</button>
                         </div>
                     </div>
-                    <div className="teamSettingModal__middle-modify">
-                        <button className="teamSettingModal__middle-modify-button">수정하기</button>
-                    </div>
+                    <hr />
                     <div className="teamSettingModal__middle-personnel">
-                        <div className="teamSettingModal__middle-personnel-current">현재 팀원</div>
-                        <div className="teamSettingModal__middle-personnel-waiting">가입 대기자</div>
+                        <div className="teamSettingModal__middle-personnel-current">
+                            <div>팀원 현황</div>
+                            <div className="teamSettingModal__middle-personnel-current-list">
+                                <MemberListItem />
+                                <MemberListItem />
+                                <MemberListItem />
+                                <MemberListItem />
+                                <MemberListItem />
+                                <MemberListItem />
+                            </div>
+                        </div>
+                        <div className="teamSettingModal__middle-personnel-waiting">
+                            <div>가입 대기자</div>
+                            <div className="teamSettingModal__middle-personnel-waiting-list">
+                                <WaitingMemberListItem />
+                                <WaitingMemberListItem />
+                                <WaitingMemberListItem />
+                                <WaitingMemberListItem />
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div className="teamSettingModal__bottom">
@@ -70,8 +94,36 @@ export function TeamSettingModal({ teamId, memberId, teamName, teamInfo, members
                         >
                             팀삭제하기
                         </button>
+                        <button
+                            onClick={() => {
+                                dispatch(closeTeamSettingModal());
+                            }}
+                        >
+                            나가기
+                        </button>
                     </div>
                 </div>
+            </div>
+        </div>
+    );
+}
+
+function MemberListItem() {
+    return (
+        <div className="memberListItem">
+            <div className="memberListItem-name">멤버이름</div>
+            <button className="memberListItem-ban">추방</button>
+        </div>
+    );
+}
+
+function WaitingMemberListItem() {
+    return (
+        <div className="waitingMemberListItem">
+            <div className="waitingMemberListItem-name">멤버이름</div>
+            <div className="waitingMemberListItem-buttons">
+                <button className="waitingMemberListItem-buttons-button">승인</button>
+                <button className="waitingMemberListItem-buttons-button">거절</button>
             </div>
         </div>
     );
