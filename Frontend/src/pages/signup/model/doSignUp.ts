8f41a630b09beb2b52921ee6642389b053a2da8f
김동학 <kdh19583@gmail.com>
2024-04-03 06:31:17 +0900

    [feat] 사용자 은행API키 발급 적용

--- Frontend/src/pages/signup/model/doSignUp.ts
+++ Frontend/src/pages/signup/model/doSignUp.ts
@@ -3,3 +3,7 @@ import { customAxios } from '@/shared';
 export async function doSignUp(userData: object) {
     return await customAxios.post('/member/signup', userData).catch((err) => err);
 }
+
+export async function createUserKey(email: string) {
+    return await customAxios.post(`/bank/user/key/${email}`).catch((err) => err);
+}
