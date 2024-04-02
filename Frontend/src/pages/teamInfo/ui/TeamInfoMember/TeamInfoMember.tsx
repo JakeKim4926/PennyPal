ede9d9e19a4dec9476c7fc43a44dc93cea2c21f2
조한빈 <whgksqls9999@naver.com>
2024-04-03 00:28:01 +0900

    [design] 팀 멤버 영역 출력 방식 수정

--- Frontend/src/pages/teamInfo/ui/TeamInfoMember/TeamInfoMember.tsx
+++ Frontend/src/pages/teamInfo/ui/TeamInfoMember/TeamInfoMember.tsx
@@ -3,7 +3,7 @@ import { TeamInfoMemberItem } from '../TeamInfoMemberItem/TeamInfoMemberItem';
 
 type TeamInfoMember = {
     teamLeaderId: number;
-    teamMembers: [];
+    teamMembers: Member[];
 };
 
 type Member = {
@@ -11,40 +11,21 @@ type Member = {
     teamLeaderId: number;
     memberThisTotalExpenses: number;
     memberId: number;
-    // id: number;
 };
 
 export function TeamInfoMember({ teamLeaderId, teamMembers }: TeamInfoMember) {
-    const [memberList, setMemberList] = useState<Member[]>([]);
-
-    useEffect(() => {
-        let members = Array(6).fill({
-            memberId: -1,
-            memberNickname: '',
-            memberLastTotalExpenses: -1,
-            memberThisTotalExpenses: -1,
-        });
-
-        for (let i = 0; i < teamMembers.length; i++) {
-            members[i] = teamMembers[i];
-        }
-
-        setMemberList([...members]);
-    }, [teamLeaderId, teamMembers]);
-
     return (
         <div className="contentCard teamInfoMember">
             <div className="contentCard__title teamInfoMember__title">
                 <div className="contentCard__title-text teamInfoMember__title_text">MEMBERS</div>
             </div>
             <div className="teamInfoMember__itemList">
-                {memberList &&
-                    memberList.map((it) => (
+                {teamMembers &&
+                    teamMembers.map((it) => (
                         <TeamInfoMemberItem
                             nickname={it.memberNickname}
                             isLeader={it.memberId === teamLeaderId}
                             spend={it.memberThisTotalExpenses}
-                            isEmpty={it.memberId === -1}
                             key={it.memberId}
                         />
                     ))}
