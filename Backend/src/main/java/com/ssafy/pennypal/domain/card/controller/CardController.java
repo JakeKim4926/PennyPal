8c9ed5b6b27973edaaa92a7e6bc5e90cfaea6707
김민건 <mine702@naver.com>
2024-04-02 14:51:55 +0900

    검색 기능 완료

--- Backend/src/main/java/com/ssafy/pennypal/domain/card/controller/CardController.java
+++ Backend/src/main/java/com/ssafy/pennypal/domain/card/controller/CardController.java
@@ -1,19 +1,18 @@
 package com.ssafy.pennypal.domain.card.controller;
 
 
+import com.ssafy.pennypal.domain.card.dto.request.SearchCard;
 import com.ssafy.pennypal.domain.card.dto.response.CardResponse;
 import com.ssafy.pennypal.domain.card.service.CardService;
 import com.ssafy.pennypal.global.common.api.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RequiredArgsConstructor
 @RequestMapping("api/card")
@@ -23,9 +22,9 @@ public class CardController {
 
     @GetMapping
     public ApiResponse<Page<CardResponse>> getCards(
-            @PageableDefault(page = 0, size = 4)
-            Pageable pageable
+            @ModelAttribute SearchCard searchCard,
+            @PageableDefault(page = 0, size = 5) Pageable pageable
     ) {
-        return cardService.getCards(pageable);
+        return cardService.getCards(pageable, searchCard);
     }
 }
