64df9ddda490072354bb732165527c08a01c0440
조한빈 <whgksqls9999@naver.com>
2024-04-03 14:09:19 +0900

    [fix] 상품 카테고리 검색

--- Frontend/src/pages/market/ui/MarketCategory/MarketCategory.tsx
+++ Frontend/src/pages/market/ui/MarketCategory/MarketCategory.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { getMarketItemByCategory } from '@/pages/market/api/getMarketItemByCategory';
 import { useDispatch } from 'react-redux';
+import { useState, useEffect } from 'react';
 import { setMarketItemList } from '../../model';
 import { getMarketItemList } from '../../model/getMarketItemList';
 
 export function MarketCategory() {
     const dispatch = useDispatch();
+    const [category, setCategory] = useState('');
     const categories = [
         {
             icon: '아이콘',
             title: '전체',
-            category: '*',
+            category: '',
         },
         {
             icon: '아이콘',
@@ -29,37 +31,33 @@ export function MarketCategory() {
         },
     ];
 
+    useEffect(() => {
+        if (category === '') {
+            getMarketItemList(0).then((res) => {
+                if (res.status === 200) {
+                    dispatch(setMarketItemList(res.data.content));
+                }
+            });
+        } else {
+            getMarketItemByCategory(category).then((res) => {
+                if (res.status === 200) {
+                    dispatch(setMarketItemList(res.data.content));
+                }
+            });
+        }
+    }, [category]);
+
     return (
         <div className="marketCategory">
-            {categories.map((it) =>
-                it.title === '전체' ? (
-                    <button
-                        className="marketCategory__item"
-                        onClick={async () => {
-                            const res = await getMarketItemList(0);
-                            if (res.status === 200) {
-                                dispatch(setMarketItemList(res.data.content));
-                            }
-                        }}
-                    >
-                        <div className="marketCategory__item-icon">{it.icon}</div>
-                        <div className="marketCategory__item-title">{it.title}</div>
-                    </button>
-                ) : (
-                    <button
-                        className="marketCategory__item"
-                        onClick={async () => {
-                            const res = await getMarketItemByCategory(it.category);
-                            if (res.status === 200) {
-                                dispatch(setMarketItemList(res.data.content));
-                            }
-                        }}
-                    >
-                        <div className="marketCategory__item-icon">{it.icon}</div>
-                        <div className="marketCategory__item-title">{it.title}</div>
-                    </button>
-                ),
-            )}
+            {categories.map((it) => (
+                <button
+                    className={`marketCategory__item ${category === it.category ? 'bold' : ''}`}
+                    onClick={() => setCategory(it.category)}
+                >
+                    <div className="marketCategory__item-icon">{it.icon}</div>
+                    <div className="marketCategory__item-title">{it.title}</div>
+                </button>
+            ))}
         </div>
     );
 }
